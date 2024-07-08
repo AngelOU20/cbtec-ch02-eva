5f0d052897b3b86666308f308494f35ab1239d70
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-08 17:28:15 -0500

    add around aspect method

--- src/main/java/edu/cibertec/capitulo02evaluacion/LoggingAspect.java
+++ src/main/java/edu/cibertec/capitulo02evaluacion/LoggingAspect.java
@@ -1,6 +1,7 @@
 package edu.cibertec.capitulo02evaluacion;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
@@ -15,4 +16,18 @@ public class LoggingAspect {
     public void actionAfter(JoinPoint joinPoint) {
         System.out.println("Acción: Ejecución después del método" + joinPoint.getSignature().getName());
     }
+
+    public Object actionAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        Long startTime = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        Long endTime = System.currentTimeMillis();
+
+        Long executionTime = endTime - startTime;
+
+        System.out.println("Tiempo empleado por " + joinPoint.getSignature().getName()
+                + " es de " + executionTime + " ms");
+        System.out.println("**********************************************");
+        return result;
+    }
+
 }
